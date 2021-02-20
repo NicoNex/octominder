f816e670e332c6c0349d6a7ffdc9ae5fc3e80016
Nicolò Santamaria <nicolo.santamaria@protonmail.com>
2021-02-20 22:28:27 +0100

    Add license header.

--- main.go
+++ main.go
@@ -1,3 +1,21 @@
+/*
+ * Octominder - a simple reminder.
+ * Copyright (C) 2021  Nicolò Santamaria
+ *
+ * Octominder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Octominder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package main
 
 import (
@@ -32,7 +50,7 @@ func main() {
 		return
 	}
 
-	logpath := filepath.Join(os.Getenv("HOME"), ".log", "otcominder.log")
+	logpath := filepath.Join(os.Getenv("HOME"), ".log", "octominder.log")
 	logfile, err := os.OpenFile(logpath, os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		fmt.Println(err)
